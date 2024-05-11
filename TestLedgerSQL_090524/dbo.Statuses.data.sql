ad0e0fa1afab1251a098c986c47a8c3daed9fa5b
Vetal <vvvv@vvvv.vv>
2024-05-11 17:29:34 +0300

    Creating complex position

---
+++ TestLedgerSQL_090524/dbo.Statuses.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Statuses] ON
+INSERT INTO [dbo].[Statuses] ([Id], [StatusName], [StatusNotes]) VALUES (4, N'Closed', N'Позиция закрыта')
+INSERT INTO [dbo].[Statuses] ([Id], [StatusName], [StatusNotes]) VALUES (5, N'Open', N'Позиция открыта')
+INSERT INTO [dbo].[Statuses] ([Id], [StatusName], [StatusNotes]) VALUES (6, N'Deleted', N'Позиция позиция удалена')
+SET IDENTITY_INSERT [dbo].[Statuses] OFF
